710820b02688957fb3251f2bbdbfe9107eeff666
ilterates <iltates@gmail.com>
2016-05-30 18:19:05 -0700

    created new user function and route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,12 @@ class UserController < ApplicationController
   def index
 
   end
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "user#index"
 
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
 
 end

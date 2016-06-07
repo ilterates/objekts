d237804049e089cd53ef1b7c9d537e996e5e23ff
ilterates <iltates@gmail.com>
2016-06-07 16:19:03 -0700

    precompiled

---
+++ public/assets/welcome-b1c37c881aaef9b848a3e9a4b15e0e06db320dfbebed3a6a0e209ec2fd3c7407.js
@@ -0,0 +1,33 @@
+
+  var wScene = new THREE.Scene();
+  var wCamera = new THREE.PerspectiveCamera( 75, 400 / 400, 0.1, 1000 );
+  wCamera.position.z = 1;
+
+  var wRenderer = new THREE.WebGLRenderer();
+  wRenderer.setClearColor ( 0xFFFFFF );
+  wRenderer.setSize( 400, 400 );
+    if ( document.getElementById( "welcome-sphere" ) ) {
+      var welcome = document.getElementById( "welcome-sphere" );
+      document.body.appendChild( welcome );
+      welcome.appendChild( wRenderer.domElement );
+    }
+
+  var wGeometry = new THREE.SphereGeometry( 0.5 );
+  wObject = new THREE.Mesh( wGeometry, new THREE.MeshBasicMaterial ({
+    color: Math.random() * 0xFFFFFF,
+    wireframe: true
+  } ) );
+  wScene.add ( wObject );
+  function render () {
+
+    requestAnimationFrame( render );
+    wObject.rotation.x += 0.01;
+    wObject.rotation.y += 0.01;
+    wObject.position.z += 0.01;
+    if (wObject.position.z >= 1 ) {
+      wObject.position.z = -5;
+    }
+
+    wRenderer.render( wScene, wCamera );
+  }
+  render();

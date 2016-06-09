80424d16df3e81d08de760ac7a72875d2b8147fa
ilterates <iltates@gmail.com>
2016-06-09 13:17:08 -0700

    altered score table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609193124) do
+ActiveRecord::Schema.define(version: 20160609201435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "scores", force: :cascade do |t|
+    t.integer   "top_score"
+    t.username  "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

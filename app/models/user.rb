2e993b524977ecbecd51680de41c7f08bce3134d
ilterates <iltates@gmail.com>
2016-06-09 12:14:05 -0700

    removed user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
 
   has_many :scores
-  has_secure_password
+  # has_secure_password
+  #
+  # validates :username, presence: true, uniqueness: true, length: { in: 3..3 }
+  # validates :password, presence: true
 
-  validates :username, presence: true, uniqueness: true, length: { in: 3..3 }
-  validates :password, presence: true
-
-  def self.confirm(params)
-    @user = User.find_by({username: params[:username]})
-    @user.try(:authenticate, params[:password])
-  end
+  # def self.confirm(params)
+  #   @user = User.find_by({username: params[:username]})
+  #   @user.try(:authenticate, params[:password])
+  # end
 end

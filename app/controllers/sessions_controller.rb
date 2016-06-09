3439e878c03afa0bec4d5e10c2e460ff83119f47
ilterates <iltates@gmail.com>
2016-06-09 11:20:10 -0700

    commented out sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
 class SessionsController < ApplicationController
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    user_params = params.require(:user).permit(:username, :password)
-    @user = User.confirm(user_params)
-    if @user
-      login(@user)
-      redirect_to @user
-    else
-      redirect_to new_session_path
-    end
-  end
+  # def create
+  #   user_params = params.require(:user).permit(:username, :password)
+  #   @user = User.confirm(user_params)
+  #   if @user
+  #     login(@user)
+  #     redirect_to @user
+  #   else
+  #     redirect_to new_session_path
+  #   end
+  # end
 
-  def destroy
-    logout
-    redirect_to root_path
-  end
+  # def destroy
+  #   logout
+  #   redirect_to root_path
+  # end
 end

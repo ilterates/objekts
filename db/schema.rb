c34d197283374a548bb0be669915d1a9b34387d2
ilterates <iltates@gmail.com>
2016-06-09 12:55:35 -0700

    removed user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531183600) do
+ActiveRecord::Schema.define(version: 20160609193124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,17 +20,6 @@ ActiveRecord::Schema.define(version: 20160531183600) do
     t.integer  "user_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
-  add_index "scores", ["user_id"], name: "index_scores_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_foreign_key "scores", "users"
 end

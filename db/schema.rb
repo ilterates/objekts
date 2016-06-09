b3b42e9e4b7fc0f61d8d3ecf7252d27e48b60137
ilterates <iltates@gmail.com>
2016-06-09 13:13:20 -0700

    removed score table

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,4 @@ ActiveRecord::Schema.define(version: 20160609193124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "scores", force: :cascade do |t|
-    t.integer  "user_score"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end

fbbcb49311bf20d734124c374b3fe2db6f4bb08a
ilterates <iltates@gmail.com>
2016-06-09 13:19:08 -0700

    removed Score table

--- db/migrate/20160609201435_create_scores.rb
+++
@@ -1,9 +0,0 @@
-class CreateScores < ActiveRecord::Migration
-  def change
-    create_table :scores do |t|
-      t.string :top_score
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,4 @@ ActiveRecord::Schema.define(version: 20160609201435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "scores", force: :cascade do |t|
-    t.integer   "top_score"
-    t.username  "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
